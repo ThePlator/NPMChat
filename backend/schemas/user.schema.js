96a5867feefff40bf9b05d196eca2cf8c04f12fd
V3DxNT <ved4geeks@gmail.com>
2026-05-19 10:31:14 +0530

    fix : Signup Schema with proper Zod validations

--- backend/schemas/user.schema.js
+++ backend/schemas/user.schema.js
@@ -2,7 +2,7 @@
 import { z } from "zod"
 
 export const signupSchema = z.object({
-  fullName: z
+  name: z
     .string({ required_error: "Full name is required" })
     .trim()
     .min(2, "Name must be at least 2 characters long"),
@@ -13,6 +13,8 @@ export const signupSchema = z.object({
   password: z
     .string({ required_error: "Password is required" })
     .min(6, "Password must be at least 6 characters long"),
+  avatarUrl: z.string().optional(),
+  bio: z.string().optional(),
 })
 
 export const loginSchema = z.object({
