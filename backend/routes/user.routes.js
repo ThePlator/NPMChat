74f8517fab09893da2e8d23f72fb597743066151
Raghav Singh <actualrag@gmail.com>
2026-05-24 22:29:31 +0530

    final fixes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,13 +6,10 @@ import {
   resetPassword,
   signup,
   updateProfile,
-<<<<<<< HEAD
   refresh,
   logout,
-=======
   sendOTP,
   verifyOTP,
->>>>>>> upstream/main
 } from "../controllers/user.controller.js"
 import { protectRoute } from "../middleware/auth.js"
 import { validateBody } from "../middleware/validate.middleware.js"
@@ -29,15 +26,12 @@ const userRouter = express.Router()
 
 userRouter.post("/signup", validateBody(signupSchema), signup)
 userRouter.post("/login", validateBody(loginSchema), login)
-<<<<<<< HEAD
 userRouter.post("/refresh", refresh)
 userRouter.post("/logout", logout)
-=======
 userRouter.post("/send-otp", validateBody(sendOTPSchema), sendOTP)
 userRouter.post("/verify-otp", validateBody(verifyOTPSchema), verifyOTP)
 userRouter.post("/forgot-password", validateBody(forgotPasswordSchema), forgotPassword)
 userRouter.post("/reset-password", validateBody(resetPasswordSchema), resetPassword)
->>>>>>> upstream/main
 userRouter.get("/check-auth", protectRoute, checkAuth)
 userRouter.put("/update-profile", protectRoute, updateProfile)
 
